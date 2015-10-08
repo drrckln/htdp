2b236a3c30ce9fa5bc060dcc60db9a6d5f7b9b5b
Derrick Lin <drrckln@gmail.com>
2015-10-08 11:18:48 -0400

    got to 4-4 Intervals

--- part1chap4.rkt
+++ part1chap4.rkt
@@ -2,6 +2,8 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname part1chap4) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ;part1chap4.rkt
+(require 2htdp/image)
+(require 2htdp/universe)
 
 (define (reward s)
   (cond
@@ -9,25 +11,89 @@
     [(and (< 10 s) (<= s 20)) "silver"]
     [else "gold"]))
 
+
+(define (next traffic-light-state)
+  (cond
+    [(string=? "red" traffic-light-state) "green"]
+    [(string=? "green" traffic-light-state) "yellow"]
+    [(string=? "yellow" traffic-light-state) "red"]))
+
+(reward 18)
+
 (define y 210)
 
 (- 200 (cond
          [(> y 200) 0]
          [else y]))
 
-(define (create-rocket-scene.v5 h)
+;(define (create-rocket-scene.v5 h)
+;  (cond
+;    [(<= h ROCKET-CENTER-TO-BOTTOM)
+;     (place-image ROCKET 50 h MTSCN)]
+;    [(> h ROCKET-CENTER-TO-BOTTOM)
+;     (place-image ROCKET 50 ROCKET-CENTER-TO-BOTTOM MTSCN)]))
+
+;(define (create-rocket-scene.v6 h)
+;  (place-image ROCKET
+;               50
+;               (cond
+;                 [(<= h ROCKET-CENTER-TO-BOTTOM) h]
+;                 [(> h ROCKET-CENTER-TO-BOTTOM) ROCKET-CENTER-TO-BOTTOM])
+;               MTSCN))
+
+; A MouseEvt is one of these strings:
+; - "button-down"
+; - "button-up"
+; - "drag"
+; - "move"
+; - "enter"
+; - "leave"
+
+; A TrafficLight shows one of three colors:
+; - "red"
+; - "green"
+; - "yellow"
+; interpretation each element of TrafficLight represents which colored
+; bulb is currently turned on
+
+; TrafficLight -> TrafficLight
+; determines the next state of the traffic light from the given s
+
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+
+(define (traffic-light-next s)
   (cond
-    [(<= h ROCKET-CENTER-TO-BOTTOM)
-     (place-image ROCKET 50 h MTSCN)]
-    [(> h ROCKET-CENTER-TO-BOTTOM)
-     (place-image ROCKET 50 ROCKET-CENTER-TO-BOTTOM MTSCN)]))
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
 
+; LightState is a String
+; interpretation the current state of the traffic light
 
-(define (create-rocket-scene.v5' h)
-  (place-image ROCKET
-               50
-               (cond
-                 [(<= h ROCKET-CENTER-TO-BOTTOM) h]
-                 [else ROCKET-CENTER-TO-BOTTOM])
-               MTSCN))
+(define (main ls)
+  (big-bang ls
+    [on-tick tock]
+    [to-draw render]))
+
+; LightState -> LightState
+; updates the state of the light
+(define (tock ls)
+  (traffic-light-next ls))
+
+; LightState -> Image
+(define (render ls)
+  (place-image (circle 10 "solid" ls) 50 50 BACKGROUND))
+
+(define BACKGROUND (empty-scene 100 100))
+
+; A 1String is a string of length 1,
+; including
+; - "\\" (the backslash),
+; - " " (the space bar),
+; - "\t" (tab),
+; - "\r" (return), and
+; - "\b" (backspace).
+; interpretation represents a single letter or keyboard character.
 
