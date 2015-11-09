7d9746f5ddff9b4b60c1ccf166f7511a5fc14326
Derrick Lin <drrckln@gmail.com>
2015-11-09 00:07:43 -0500

    exercise 144

--- part2chap10.rkt
+++ part2chap10.rkt
@@ -1,6 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname part2chap10) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
 ; by the first clause
 '()
 ; by the second clause and the preceding example
@@ -134,3 +135,52 @@
                   (= (image-width  (first loi)) n))
              (first loi)]
             [else (ill-sized? (rest loi) n)])]))
+
+; A List-of-temperatures is one of:
+; - '()
+; - (cons CTemperature List-of-temperatures)
+
+; A CTemperature is a Number greater or equal to -273.
+
+; List-of-temperatures -> Number
+; computes the average temperature
+;(define (average alot)
+;  (cond
+;    [(empty? alot) ...]
+;    [(cons? alot)
+;     (... (first alot) ... (average (rest alot)) ...)]))
+; this doesn't work though.. break into 3 functions!
+
+
+; List-of-temperatures -> Number
+; computes the average temperature
+(define (average alot)
+  (/ (sum alot)
+     (how-many alot)))
+
+(check-expect (average (cons 1 (cons 2 (cons 3 '())))) 2)
+
+; List-of-temperatures -> Number
+; adds up the temperatures on the given list
+(define (nsum alot)
+  (cond
+    [(empty? alot) 0]
+    [else (+ (first alot) (sum (rest alot)))]))
+
+(check-expect (nsum (cons 1 (cons 2 (cons 3 '())))) 6)
+
+; List-of-temperatures -> Number
+; counts the temperatures on the given list
+;(define (how-many alot) 0)
+
+; Exercise 144
+; List-of-temperatures -> Number
+; computes the average temperature
+(define (checked-average alot)
+  (cond
+    [(empty? alot) (error "list of temperatures is empty")]
+    [else (/ (sum alot)
+             (how-many alot))]))
+
+(check-error (checked-average '()))
+(check-expect (checked-average (cons 1 (cons 2 (cons 3 '())))) 2)
