8a6b385e29af188b90ce99817df3a35d0bbe47c8
Derrick Lin <drrckln@gmail.com>
2015-11-19 10:59:58 -0800

    exercise 171

--- part2chap11.rkt
+++ part2chap11.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname part2chap11) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-
+(require 2htdp/batch-io)
 (define HOURLY 12)
 
 ; Number -> Number
@@ -229,3 +229,52 @@
 ; changes area code to 281
 (define (make-281-phone p)
   (make-phone 281 (phone-switch p) (phone-four p)))
+
+; Exercise 171
+; A List-of-String is one of:
+; - '()
+; - (cons String List-of-String)
+
+(cons "TTT"
+      (cons ""
+            (cons "Put up in a place"
+                  (cons "where it's easy to see"
+                        (cons "the cryptic admonishment"
+                              (cons "T.T.T."
+                                    (cons ""
+                                          (cons "When you feel how depressingly"
+                                                (cons "slowly you climb"
+                                                      (cons "it's well to remember that"
+                                                            (cons "Things Take Time."
+                                                                  (cons ""
+                                                                        (cons "Piet Hein" '())))))))))))))
+
+(read-lines "ttt.txt")
+
+(cons "TTT" (cons "Put" (cons "up" (cons "in" (cons "a" (cons "place" (cons "where" (cons "it's" (cons "easy"
+      (cons "to" (cons "see" (cons "the" (cons "cryptic" (cons "admonishment" (cons "T.T.T." (cons "When"
+      (cons "you" (cons "feel" (cons "how" (cons "depressingly" (cons "slowly" (cons "you" (cons "climb"
+      (cons "it's" (cons "well" (cons "to" (cons "remember" (cons "that" (cons "Things" (cons "Take" (cons "Time."
+      (cons "Piet" (cons "Hein" '())))))))))))))))))))))))))))))))))
+
+(read-words "ttt.txt")
+
+; List-of-List-of-String is one of:
+; - '()
+; - (cons List-of-String List-of-List-of-String)
+
+(cons (cons "TTT" '())
+      (cons '()
+            (cons (cons "Put" (cons "up" (cons "in" (cons "a" (cons "place" '())))))
+                  (cons (cons "where" (cons "it's" (cons "easy" (cons "to" (cons "see" '())))))
+                        (cons (cons "the" (cons "cryptic" (cons "admonishment" '())))
+                              (cons (cons "T.T.T." '())
+                                    (cons '()
+                                          (cons (cons "When" (cons "you" (cons "feel" (cons "how" (cons "depressingly" '())))))
+                                                (cons (cons "slowly" (cons "you" (cons "climb" '())))
+                                                      (cons (cons "it's" (cons "well" (cons "to" (cons "remember" (cons "that" '())))))
+                                                            (cons (cons "Things" (cons "Take" (cons "Time." '())))
+                                                                  (cons '()
+                                                                        (cons (cons "Piet" (cons "Hein" '())) '())))))))))))))
+
+(read-words/line "ttt.txt")
