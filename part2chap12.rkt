41011503d6776bc2cb8ad5b95ee6d7cb9bc06bae
Derrick Lin <drrckln@gmail.com>
2015-11-24 12:43:13 -0800

    exercise 183

--- part2chap12.rkt
+++ part2chap12.rkt
@@ -12,3 +12,34 @@
               (cons (cons 1 (cons 2 '())) (cons (cons 2 '()) '())))
 (check-expect (list (list "a" 2) "hello")
               (cons (cons "a" (cons 2 '())) (cons "hello" '())))
+
+; Exercise 182
+(check-expect (list 0 1 2 3 4 5)
+              (cons 0 (cons 1 (cons 2 (cons 3 (cons 4 (cons 5 '())))))))
+(check-expect (list (list "adam" 0) (list "eve" 1) (list "louisXIV" 2))
+              (cons (cons "adam" (cons 0 '()))
+                    (cons (cons "eve" (cons 1 '()))
+                          (cons (cons "louisXIV" (cons 2 '()))
+                                '()))))
+(check-expect (list 1 (list 1 2) (list 1 2 3))
+              (cons 1 (cons (cons 1 (cons 2 '()))
+                            (cons (cons 1 (cons 2 (cons 3 '())))
+                                  '()))))
+
+; Exercise 183
+(check-expect (cons "a" (list 0 #false))
+              (cons "a"
+                    (cons 0 (cons #false '()))))
+(check-expect (list (cons 1 (cons 13 '())))
+              (cons (cons 1 (cons 13 '())) '()))
+(check-expect (cons (list 1 (list 13 '())) '())
+              (cons (cons 1
+                          (cons (cons 13 (cons '() '()))
+                                '()))
+                    '()))
+(check-expect (list '() '() (cons 1 '()))
+              (cons '() (cons '() (cons (cons 1 '()) '()))))
+(check-expect (cons "a" (cons (list 1) (list #false '())))
+              (cons "a"
+                    (cons (cons 1 '())
+                          (cons #false (cons '() '())))))
