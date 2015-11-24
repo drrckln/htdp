05543ce37176b5c90a40f2bb5bc39417e9d9df16
Derrick Lin <drrckln@gmail.com>
2015-11-24 12:48:39 -0800

    exercise 183 list versions

--- part2chap12.rkt
+++ part2chap12.rkt
@@ -43,3 +43,14 @@
               (cons "a"
                     (cons (cons 1 '())
                           (cons #false (cons '() '())))))
+
+(check-expect (cons "a" (list 0 #false))
+              (list "a" 0 #false))
+(check-expect (list (cons 1 (cons 13 '())))
+              (list (list 1 13)))
+(check-expect (cons (list 1 (list 13 '())) '())
+              (list (list 1 (list 13 '()))))
+(check-expect (list '() '() (cons 1 '()))
+              (list '() '() (list 1)))
+(check-expect (cons "a" (cons (list 1) (list #false '())))
+              (list "a" (list 1) #false '()))
