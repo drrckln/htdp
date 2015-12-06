9f3675883aeb794c8a5a2e0d9bcec946415a5bb2
Derrick Lin <drrckln@gmail.com>
2015-12-05 16:58:18 -0800

    starting exercise 201

---
+++ part2chap13worm.rkt
@@ -0,0 +1,4 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname part2chap13worm) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/universe)
