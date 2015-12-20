101ee50774a59cf1cefba9e081c56f0b94b4b5ec
Derrick Lin <drrckln@gmail.com>
2015-12-19 23:47:01 -0500

    exercise 217

---
+++ intermezzo_quote.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname intermezzo_quote) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Exercise 217
+(list 1 "a" 2 #false 3 "c")
+(list)
+
+(list (list "alan" 1000)
+      (list "barb" 2000)
+      (list "carl" 1500)
+      (list "dawn" 2300))
