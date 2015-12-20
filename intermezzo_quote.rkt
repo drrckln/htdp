c94ba67c71ce30e33020e46071ea9e17639d9d0f
Derrick Lin <drrckln@gmail.com>
2015-12-20 00:45:33 -0500

    exercise 220

--- intermezzo_quote.rkt
+++ intermezzo_quote.rkt
@@ -51,7 +51,9 @@
 ; Number -> ... nested list ...
 ; creates a cell for an HTML table from a number
 (define (make-cell n)
-  `(td ,(number->string n)))
+  (cond
+    [(string? n) `(td ,n)]
+    [(number? n) `(td ,(number->string n))]))
 
 ; List-of-numbers List-of-numbers -> ... nested list ...
 ; creates an HTML table from two lists of numbers
@@ -85,3 +87,33 @@
             (list `table (list (list `border "1"))
                   (list `tr (list (list `width "200")) (list 'td "1") (list 'td "2"))
                   (list `tr (list (list `width "200")) (list 'td "99") (list 'td "65")))))
+
+; Exercise 220
+; List-of-String -> ... list representation of an HTML table
+(define (make-ranking lost)
+  `(table ((border "1"))
+          ,@(make-rows (ranking lost))))
+
+; List-of-String -> List-of-Rows
+(define (make-rows los)
+  (cond
+    [(empty? los) '()]
+    [else (cons `(tr ,@(make-row (first los))) (make-rows (rest los)))]))
+
+(define one-list
+  '("Asia: Heat of the Moment"
+    "U2: One"
+    "The White Stripes: Seven Nation Army"))
+
+; List-of-String -> List-of-String
+; adds the ranking in
+(define (ranking los)
+  (reverse (add-ranks (reverse los))))
+
+; List-of-String -> List-of-List-of-Num-String
+; turns each string into a list with a number as the first the string as the second
+(define (add-ranks los)
+  (cond
+    [(empty? los) '()]
+    [else (cons (list (length los) (first los))
+                (add-ranks (rest los)))]))
