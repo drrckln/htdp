30938793b701cfded0474b678547d1342b38c8a0
Derrick Lin <drrckln@gmail.com>
2015-12-27 23:53:23 -0500

    exercise 245

--- part3chap18.rkt
+++ part3chap18.rkt
@@ -16,3 +16,26 @@
 ; Finds the X in the list... smallest number. Basically, it's
 ; the number where if you map f onto the List-of-X, which X
 ; gave you the largest numerical value? (if tied, give the 1st)
+
+; Exercise 245
+; [X Y -> Y] Y [List-of X] -> Y
+; my-foldl works just like foldl
+(check-expect (my-foldl cons '() '(a b c)) (foldl cons '() '(a b c)))
+(check-expect (my-foldl / 1 '(6 3 2)) (foldl / 1 '(6 3 2)))
+(define (my-foldl f e l)
+  (foldr f e (reverse l)))
+
+; N [N -> X] -> [List-of X]
+; works just like build-list
+(define (my-build-list n f)
+  (build 0 n f))
+
+; N N [N -> X] -> [List-of X]
+(define (build count n f)
+  (cond
+    [(= count n) '()]
+    [else (cons (f count)
+                (build (add1 count) n f))]))
+
+(check-expect (my-build-list 10 add1) (build-list 10 add1))
+(check-expect (my-build-list 3 sub1) (build-list 3 sub1))
