361043148bc29854587e4277309afa7f4c0a28d9
Derrick Lin <drrckln@gmail.com>
2015-12-29 15:50:30 -0500

    exercise 252 added sup with local

--- part3chap18.rkt
+++ part3chap18.rkt
@@ -282,4 +282,15 @@
 (inf (list 2 1 3))
 
 ; Exercise 252
+; Nelon -> Number
+; determines the smallest number on l
+(define (sup l)
+  (cond
+    [(empty? (rest l)) (first l)]
+    [else
+     (local ((define largest-in-rest (sup (rest l))))
+       (cond
+         [(> (first l) largest-in-rest) (first l)]
+         [else largest-in-rest]))]))
+
 (sup (list 2 1 3))
