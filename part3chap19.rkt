2b09e1d9c94fad65176a443ddcc2004b290bd130
Derrick Lin <drrckln@gmail.com>
2016-01-04 22:38:37 -0500

    exercise 266

--- part3chap19.rkt
+++ part3chap19.rkt
@@ -2,8 +2,13 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname part3chap19) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; Exercise 265
-(lambda (x y) (x y y)) ; legal if x is a function
-(lambda () 10) ; not legal, no variables
-(lambda (x) x) ; legal, just returns. identity.
-(lambda (x y) x) ; legal, like first
-(lambda x 10) ; not legal, syntax error
+;(lambda (x y) (x y y)) ; legal if x is a function
+;(lambda () 10) ; not legal, no variables
+;(lambda (x) x) ; legal, just returns. identity.
+;(lambda (x y) x) ; legal, like first
+;(lambda x 10) ; not legal, syntax error
+
+; Exercise 266
+; 1. 3
+; 2. 14
+; 3. 13.25
