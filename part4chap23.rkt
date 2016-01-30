c3aca678c7e3f082ef4da2bfd5c61587586c6a20
Derrick Lin <drrckln@gmail.com>
2016-01-30 21:59:09 +0800

    exercise 315

---
+++ part4chap23.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname part4chap23) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Exercise 315
+; read! occurs twice in the directory tree TS:
+; /TS/read!
+; /TS/Libs/Docs/read!
+; the total size of all the files is:
+; 99 + 52 + 10 + 17 + 8 + 2 + 19
+; with directories, add 5
+; TS contains 3 + 1 levels
