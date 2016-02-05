71f4272d8c736b8ccf822ce26ffb0fd1578e9539
Derrick Lin <drrckln@gmail.com>
2016-02-05 18:51:28 +0800

    exercise 331

---
+++ part4chap24.rkt
@@ -0,0 +1,24 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname part4chap24) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct add [left right])
+(define-struct mul [left right])
+
+; Exercise 331
+; A BSL-expr is one of:
+; - Number
+; - (make-add BSL-expr BSL-expr)
+; - (make-mul BSL-expr BSL-expr)
+
+(make-add 10 -10)
+(make-add (make-mul 20 3) 33)
+(make-add (make-mul 3.14
+                    (make-mul 2 3))
+          (make-mul 3.14
+                    (make-mul -1 -9)))
+
+(+ -1 2)
+(+ (* -2 -3) 33)
+(* (+ 1
+      (* 2 3))
+   (* 3.14 12))
