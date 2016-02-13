762f4a7c2448ed80f8df48faffa1628d3f87165e
Derrick Lin <drrckln@gmail.com>
2016-02-13 14:03:56 -0800

    starting xml

---
+++ part4chap25.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname part4chap25) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct element [name content])
+
+; <machine><action /><action /><action /></machine> expanded is:
+; <machine><action></action><action></action><action></action></machine>
+
+(make-element "machine" (list (make-element "action" '())))
